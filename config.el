a64b0cd749c90798cc18c19ddded1be0a909d42a
pixie <megcd93@gmail.com>
2025-10-22 20:09:39 -0600

    relative

--- config.el
+++ config.el
@@ -32,12 +32,13 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-solarized-light)
-(setq doom-font "Hack-18")
+(setq doom-theme 'doom-dracula)
+(setq doom-font "Hack-18"
+      doom-variable-pitch-font "DejaVu Serif-18" )
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
