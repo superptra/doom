dafc321210f3a27e2d9b48ba618597cf56b62591
pixie <megcd93@gmail.com>
2025-10-25 17:02:51 -0600

    hack if not Menlo

--- config.el
+++ config.el
@@ -33,7 +33,9 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-dracula)
-(if (featurep :system 'macos) (setq doom-font "Menlo-22"))
+(if (featurep :system 'macos)
+    (setq doom-font "Menlo-22")
+    (setq doom-font "Hack-18"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
